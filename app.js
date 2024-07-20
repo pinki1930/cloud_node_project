4cb6bb37404d25b26b8d79c298b81e4ae6d6496b
pinki1930 <141972548+pinki1930@users.noreply.github.com>
2024-07-20 17:39:08 -0400

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const server = http.createServer((req, res) => {
 });
 
 // The server listens on port 3000.
-const port = 3000;
+const port = process.env.PORT || 3000;
 server.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
 });
